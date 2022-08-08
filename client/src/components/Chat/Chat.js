872c63f8ca99252a1ee18e71307e33558176b262
Ali Demirci <ademirci@phinion.com>
2022-08-08 14:02:04 +0200

    Add chat.js to input field for message and header info for room

--- client/src/components/Chat/Chat.js
+++ client/src/components/Chat/Chat.js
@@ -3,11 +3,15 @@ import queryString from 'query-string';
 import io from 'socket.io-client';
 import './chat.css';
 import { useLocation } from 'react-router';
+import InfoBar from '../InfoBar/InfoBar';
+import Input from '../Input/Input';
 
 let socket;
 const Chat = () => {
   const [name, setName] = useState('');
   const [room, setRoom] = useState('');
+  const [message, setMessage] = useState('');
+  const [messages, setMessages] = useState([]);
   const ENDPOINT = 'localhost:5000';
   const location = useLocation();
 
@@ -18,18 +22,37 @@ const Chat = () => {
     setName(name);
     setRoom(room);
     // this for sending data to backend
-    socket.emit('join', {name,room},()=> {
-      
+    socket.emit('join', { name, room }, () => {
+
     });
     // unmount use effect after disconnect it close socket.
-    return ()=> {
+    return () => {
       socket.emit('disconnect');
 
       socket.off();
     }
-  },[ENDPOINT,location.search])
+  }, [ENDPOINT, location.search])
+
+  //To get messages
+  useEffect(() => {
+    socket.on('message', (message) => {
+      setMessages([...messages, message]);
+    })
+  }, [messages]);
+  const sendMessage = (e) => {
+    e.preventDefault();
+    if (message) {
+      socket.emit('sendMessage', message, () => setMessage(''))
+    }
+  }
+  console.log(message,messages)
   return (
-    <div>Chat</div>
+    <div className='outerContainer'>
+      <div className='container'>
+        <InfoBar room={room}></InfoBar>
+        <Input setMessage={setMessage} sendMessage={sendMessage} message={message}></Input>
+      </div>
+    </div>
   )
 }
 export default Chat
--- server/index.js
+++ server/index.js
@@ -4,6 +4,8 @@ const express = require('express');
 const socketio = require('socket.io');
 const cors = require('cors');
 
+const { addUser, removeUser, getUser, getUsersInRoom } = require('./users.js');
+
 const PORT = process.env.PORT || 5000;
 
 const router = require('./router');
@@ -13,16 +15,38 @@ const server = http.createServer(app);
 const io = socketio(server, { cors: { origin: "*" } }); // to avoid cors error use {cors: {origin: "*"}}
 
 io.on('connection', (socket) => {
-    console.log('New User connected')
+
+    // it is coming frontend with 'join'
+    socket.on('join', ({ name, room }, callback) => {
+        const { error, user } = addUser({ id: socket.id, name, room });
+
+        if (error) return callback(error);
+
+        // to send a message from admin when user joined to room
+        socket.emit('message', { user: 'admin', text: `${user.name} Welcome to room ${user.room}` })
+       
+       //to send message all room to inform user joined
+       socket.broadcast.to(user.room).emit('message',{user:'admin', text:`${user.name}, joined`});
+
+        //socket.join is a method that allow user to join specific room
+        socket.join(user.room);
+
+        //for now there is no error from frontend
+        callback();
+    });
+
+    socket.on('sendMessage', (message,callback)=> {
+        // get user from socket id 
+        const user= getUser(socket.id);
+        // send user message to room in backend to frontend
+        io.to(user.room).emit('message',{user:user.name,text:message});
+
+        callback();
+    })
 
     socket.on('disconnect', () => {
         console.log('One user disconnected');
     })
-
-    // it is coming frontend with 'join'
-    socket.on('join', ({ name, room },callback) => {
-        console.log(name, room);
-    });
 });
 app.use(cors());
 app.use(router);
